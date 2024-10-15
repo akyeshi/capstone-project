0a300db754a64c86c0f75b85e6de1d94f7f06207
akyeshi <akyeshi@outlook.com>
2024-10-15 15:17:16 -0400

    signin signup pages with react-router-dom setup

---
+++ src/pages/auth/SignInPage.jsx
@@ -0,0 +1,13 @@
+
+
+const SignInPage = () => {
+  return (
+    <div className="font-lato text-green-500">
+      Sign In
+      <i className="fa-solid fa-right-to-bracket"></i>
+    </div>
+  )
+}
+
+
+export default SignInPage; 
---
+++ src/pages/auth/SignUpPage.jsx
@@ -0,0 +1,12 @@
+
+
+const SignUpPage = () => {
+  return (
+    <div className="font-playfair text-blue-500">
+      Sign Up
+    </div>
+  )
+}
+
+
+export default SignUpPage; 
